1a58680c3e6b708ba431b02be9c1c5122f21ca79
Jorge Reyes <jreycruz1126@gmail.com>
2026-08-03 22:54:46 -0600

    configuracion inicial

---
+++ src/services/invoice.service.js
@@ -0,0 +1,15 @@
+export const createInvoice = async (invoiceData) => {
+
+};
+
+export const getInvoices = async (user) => {
+
+};
+
+export const getInvoiceById = async (id, user) => {
+
+};
+
+export const voidInvoice = async (id, user) => {
+
+};
