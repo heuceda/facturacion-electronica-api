64dad6e41bd95548272da4d6a15783929ef0f9f1
Helen Euceda <neuceda13@gmail.com>
2026-08-04 06:41:42 -0600

    feat: continuacion configuracion inicial

--- database.sql
+++ database.sql
@@ -1,15 +1,15 @@
--- Script de las tablas de Facturación Electrónica, para la creación de la base de datos.
+-- Script de creación de base de datos y tabla de productos.
 
 CREATE DATABASE IF NOT EXISTS facturacion_electronica_db;
 USE facturacion_electronica_db;
 
 -- Tabla de Productos
 CREATE TABLE IF NOT EXISTS products (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  code VARCHAR(50) NOT NULL UNIQUE,
-  name VARCHAR(100) NOT NULL,
-  price DECIMAL(10, 2) NOT NULL CHECK,
-  stock INT NOT NULL,
-  is_active BOOLEAN DEFAULT TRUE,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(50) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
