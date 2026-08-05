61156cc55f2d52dd99981615e588ec01271335d6
Helen Euceda <neuceda13@gmail.com>
2026-08-05 13:12:56 -0600

    fix: Correcciones para el funcionamiento de las routes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
-import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
-import productRoutes from './routes/product.routes.js';
+import app from './app.js';
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Middlewares globales
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Ruta raíz de bienvenida
-app.get('/', (req, res) => {
-  res.json({
-    message: 'API REST de Facturación Electrónica v1',
-    status: 'online'
-  });
-});
-
-// Ruta del módulo de Productos 
-app.use('/api/v1/products', productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
+
