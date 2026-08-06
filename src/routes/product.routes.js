75dade22481a322171aeeeb14c9a4ebe71f799f6
Luis-Lopez-Pineda <lf.pineda@unah.hn>
2026-08-05 22:38:00 -0600

    Actualizacion del index y correccion de nombre en rutas de productos

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -14,4 +14,4 @@ Productrouter.post('/', isAuth, isAdmin, createProduct);
 // PATCH - Aumentar inventario de un producto 
 Productrouter.patch('/:id/stock', isAuth, isAdmin, increaseStock);
 
-export default router;
+export default Productrouter;
